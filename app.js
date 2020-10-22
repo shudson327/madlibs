6dc33eff2c3fe201586072c11ce14c5abdd628b6
SierraHudson <SierraHudson327@gmail.com>
2020-10-22 19:32:59 -0400

    added javascript

---
+++ app.js
@@ -0,0 +1,66 @@
+
+function createParagraph() {
+    console.log('create paragraph has been called');
+    var wordOne = document.getElementById('one').value;
+    var wordTwo = document.getElementById('two').value;
+    var wordThree = document.getElementById('three').value;
+    var wordFour = document.getElementById('four').value;
+    var wordFive = document.getElementById('five').value;
+    var wordSix = document.getElementById('six').value;
+    var wordSeven = document.getElementById('seven').value;
+    var wordEight = document.getElementById('eight').value;
+    var wordNine = document.getElementById('nine').value;
+    var wordTen = document.getElementById('ten').value;
+    var wordEleven = document.getElementById('eleven').value;
+    var wordTwelve = document.getElementById('twelve').value;
+    var wordThirteen = document.getElementById('thirteen').value;
+    var wordFourteen = document.getElementById('fourteen').value;
+    var wordFifteen = document.getElementById('fifteen').value;
+    var wordSixteen = document.getElementById('sixteen').value;
+    var wordSeventeen = document.getElementById('seventeen').value;
+    var wordEighteen = document.getElementById('eighteen').value;
+    var wordNineteen = document.getElementById('nineteen').value;
+    var wordTwenty = document.getElementById('twenty').value;
+    var wordTwentyone = document.getElementById('twentyone').value;
+    var wordTwentytwo = document.getElementById('twentytwo').value;
+
+
+    var paragraph = '<p> Last night I went to the most ' + wordOne + 'Halloween party! The invitation was delivered by ' + wordTwo + 'and told me to go to the deep dark ' + wordThree + 'hello!.</p>';
+    
+    console.log(paragraph);
+    document.getElementById('answer').innerHTML = paragraph;
+
+}
+
+    // Last night I went to the most _________ Halloween
+    //         party! The invitation was delivered by ____________
+    //         and told me to go to the deep dark ______________
+    //         and _________ all the way to the very top of the
+    //         spooky __________. I __________ the doorbell
+    //         __________. ___________! My friend __________ answered
+    //         the door dressed up as _____________ and said
+    //         “______________!” There were _________ different
+    //         costumes, including _____________ ghouls and mummified
+    //         _____________. The music was loud and ___________, so
+    //         I ______________ until my __________ hurt. There were
+    //         also delicious treats to __________, like candy corn and
+    //         chocolate-covered ______________, but my favourite part of
+    //         the party was the pumpkin _____________ and bobbing for
+    //         ______________. I can’t wait until next Halloween—
+    //         it will be even more ______________!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
